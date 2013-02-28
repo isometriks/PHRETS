b32dcabbcc6e35b76ea98aaefcd698d11f24bb30
Craig Blanchette <craig.blanchette@gmail.com>
2013-02-28 17:12:43 -0500

    For GetMetadata requests.

---
+++ Result/LookupResult.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace PHRETS\Result;
+
+class LookupResult extends AbstractResult implements \IteratorAggregate
+{
+    protected $lookup; 
+    protected $resource; 
+    protected $date; 
+    protected $version; 
+    
+    /**
+     * Force the SimpleXMLElement to an array
+     * 
+     * TODO: Do we want to change the array keys to be one of the lookup
+     * values so you can find it easily?
+     * 
+     * @param type $result
+     */
+    public function addResult($result)
+    {
+        parent::addResult((array)$result);
+    }
+    
+    public function getLookup()
+    {
+        return $this->lookup; 
+    }
+    
+    public function setLookup($lookup)
+    {
+        $this->lookup = $lookup; 
+    }
+    
+    public function getResource()
+    {
+        return $this->resource; 
+    }
+    
+    public function setResource($resource)
+    {
+        $this->resource = $resource; 
+    }
+    
+    public function getDate()
+    {
+        return $this->date; 
+    }
+    
+    public function setDate($date)
+    {
+        $this->date = $date; 
+    }
+    
+    public function getVersion()
+    {
+        return $this->version; 
+    }
+    
+    public function setVersion($version)
+    {
+        $this->version = $version; 
+    }
+
+}
