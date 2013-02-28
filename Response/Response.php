5286b4a11419d70bd182d9ccb1f041630c6b75f7
Craig Blanchette <craig.blanchette@gmail.com>
2013-02-28 12:54:00 -0500

    Fixed how responses work

---
+++ Response/Response.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHRETS\Response;
+
+class Response extends AbstractResponse
+{
+    public function hasError()
+    {
+        return false;
+    }
+}
---
+++ Response/XmlResponse.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PHRETS\Response; 
+
+class XmlResponse extends Response
+{
+    public function hasError()
+    {
+        return $this->getReplyCode() !== 0; 
+    }
+    
+    public function getReplyCode()
+    {
+        $body = $this->getBody(); 
+        return (int)$body['ReplyCode'];
+    }
+    
+    public function getReplyText()
+    {
+        $body = $this->getBody(); 
+        return (string)$body['ReplyText'];
+    }
+    
+    public function getRetsResponse()
+    {
+        $body = $this->getBody(); 
+        return (string)$body->{'RETS-RESPONSE'};
+    }    
+}
